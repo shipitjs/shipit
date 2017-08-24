a2169fd264fd624530b5e25951f43edf0fded64d
Greg Bergé <berge.greg@gmail.com>
2017-08-25 00:24:46 +0200

    chore: make ESLint happy

--- packages/ssh-pool/examples/hostname.js
+++ packages/ssh-pool/examples/hostname.js
@@ -1,9 +1,11 @@
-var sshPool = require('../');
+const sshPool = require('../')
 
-var pool = new sshPool.ConnectionPool(['neoziro@localhost', 'neoziro@localhost']);
+const pool = new sshPool.ConnectionPool([
+  'neoziro@localhost',
+  'neoziro@localhost',
+])
 
-pool.run('hostname')
-.then(function (results) {
-  console.log(results[0].stdout);
-  console.log(results[1].stdout);
-});
+pool.run('hostname').then(results => {
+  console.log(results[0].stdout)
+  console.log(results[1].stdout)
+})
