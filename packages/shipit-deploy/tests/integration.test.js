29ab6b2bf4c7d83e660efa7e2b401e866c6cb1e3
Greg Bergé <berge.greg@gmail.com>
2018-03-25 17:41:07 +0200

    tests: adjust timeout

--- packages/shipit-deploy/tests/integration.test.js
+++ packages/shipit-deploy/tests/integration.test.js
@@ -7,26 +7,32 @@ const shipitFile = path.resolve(__dirname, './sandbox/shipitfile.babel.js')
 jest.setTimeout(40000)
 
 describe('shipit-cli', () => {
-  it('should run a local task', async () => {
-    await exec(`babel-node ${shipitCli} --shipitfile ${shipitFile} test deploy`)
+  it(
+    'should run a local task',
+    async () => {
+      await exec(
+        `babel-node ${shipitCli} --shipitfile ${shipitFile} test deploy`,
+      )
 
-    const { stdout: lsReleases } = await exec(
-      `babel-node ${shipitCli} --shipitfile ${shipitFile} test ls-releases`,
-    )
+      const { stdout: lsReleases } = await exec(
+        `babel-node ${shipitCli} --shipitfile ${shipitFile} test ls-releases`,
+      )
 
-    const latestRelease = lsReleases
-      .split('\n')
-      .reverse()[2]
-      .match(/\d{14}/)[0]
+      const latestRelease = lsReleases
+        .split('\n')
+        .reverse()[2]
+        .match(/\d{14}/)[0]
 
-    const { stdout: lsCurrent } = await exec(
-      `babel-node ${shipitCli} --shipitfile ${shipitFile} test ls-current`,
-    )
+      const { stdout: lsCurrent } = await exec(
+        `babel-node ${shipitCli} --shipitfile ${shipitFile} test ls-current`,
+      )
 
-    const currentRelease = lsCurrent
-      .split('\n')[3]
-      .match(/releases\/(\d{14})/)[1]
+      const currentRelease = lsCurrent
+        .split('\n')[3]
+        .match(/releases\/(\d{14})/)[1]
 
-    expect(latestRelease).toBe(currentRelease)
-  })
+      expect(latestRelease).toBe(currentRelease)
+    },
+    20000,
+  )
 })
--- packages/ssh-pool/tests/integration.test.js
+++ packages/ssh-pool/tests/integration.test.js
@@ -7,22 +7,30 @@ describe('ssh-pool', () => {
 
   beforeEach(() => {
     pool = new sshPool.ConnectionPool(['deploy@test.shipitjs.com'], {
-      key: './ssh/id_rsa',
+      key: path.resolve(__dirname, '../../../ssh/id_rsa'),
     })
   })
 
-  it('should run a command remotely', async () => {
-    const [{ stdout }] = await pool.run('hostname')
-    expect(stdout).toBe('shipit-test\n')
-  })
+  it(
+    'should run a command remotely',
+    async () => {
+      const [{ stdout }] = await pool.run('hostname')
+      expect(stdout).toBe('shipit-test\n')
+    },
+    10000,
+  )
 
-  it('should escape command properly', async () => {
-    const [{ stdout: first }] = await pool.run('echo $USER')
-    expect(first).toBe('deploy\n')
+  it(
+    'should escape command properly',
+    async () => {
+      const [{ stdout: first }] = await pool.run('echo $USER')
+      expect(first).toBe('deploy\n')
 
-    const [{ stdout: second }] = await pool.run("echo '$USER'")
-    expect(second).toBe('$USER\n')
-  })
+      const [{ stdout: second }] = await pool.run("echo '$USER'")
+      expect(second).toBe('$USER\n')
+    },
+    10000,
+  )
 
   it(
     'should copy to remote',
