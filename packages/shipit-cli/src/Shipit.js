a03512bceabe5270e3efe553aef97cbcb3853e43
Dirk Elmendorf <dirk@r26d.com>
2019-02-23 08:35:07 -0600

    Change to be more explict

--- packages/shipit-cli/src/Shipit.js
+++ packages/shipit-cli/src/Shipit.js
@@ -166,7 +166,7 @@ class Shipit extends Orchestrator {
 
     this.pool = new ConnectionPool(servers, options)
 
-    this.emit('init:ssh_pool')
+    this.emit('init:after_ssh_pool')
     return this
   }
 
