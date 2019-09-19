80f2347fa4b289144faf3edd8a17f3aaa0fb72a5
Shenfq <shenfq95@foxmail.com>
2019-09-20 00:44:33 +0800

    fix: after cd, must run the specified drive letter

--- packages/ssh-pool/src/commands/cd.js
+++ packages/ssh-pool/src/commands/cd.js
@@ -3,5 +3,11 @@ import { joinCommandArgs, requireArgs } from './util'
 export function formatCdCommand({ folder }) {
   requireArgs(['folder'], { folder }, 'cd')
   const args = ['cd', folder]
+  const isWin = /^win/.test(process.platform)
+  const startStr = folder.slice(0, 1)
+  if (isWin && folder.indexOf('/') > 0) {
+    const drive = folder.slice(0, 2)
+    args.push(`&& ${drive}`)
+  }
   return joinCommandArgs(args)
 }
