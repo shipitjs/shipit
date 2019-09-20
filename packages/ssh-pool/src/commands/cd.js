14e05b3cd411781a81fdb506cf60d40ba34ed869
Shenfq <shenfq95@foxmail.com>
2019-09-20 08:47:33 +0800

    fix: lint fix

--- packages/ssh-pool/src/commands/cd.js
+++ packages/ssh-pool/src/commands/cd.js
@@ -4,7 +4,6 @@ export function formatCdCommand({ folder }) {
   requireArgs(['folder'], { folder }, 'cd')
   const args = ['cd', folder]
   const isWin = /^win/.test(process.platform)
-  const startStr = folder.slice(0, 1)
   if (isWin && folder.indexOf('/') > 0) {
     const drive = folder.slice(0, 2)
     args.push(`&& ${drive}`)
