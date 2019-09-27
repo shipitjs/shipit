ab916a98550e326b64c09beab50741e48891973e
自然醒 <shenfq95@foxmail.com>
2019-09-27 15:44:52 +0800

    fix(windows): cd must run the specified drive letter (#252)

--- packages/ssh-pool/src/commands/cd.js
+++ packages/ssh-pool/src/commands/cd.js
@@ -1,7 +1,15 @@
+import path from 'path'
 import { joinCommandArgs, requireArgs } from './util'
+
+const isWin = /^win/.test(process.platform)
 
 export function formatCdCommand({ folder }) {
   requireArgs(['folder'], { folder }, 'cd')
   const args = ['cd', folder]
+  const { root } = path.parse(folder)
+  const drive = root.replace(path.sep, '')
+  if (isWin && root !== '/') {
+    args.push(`&& ${drive}`)
+  }
   return joinCommandArgs(args)
 }
