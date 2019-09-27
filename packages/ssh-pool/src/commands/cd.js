a3091ffc62956e1a98afec111de9a83d651a58af
dawu <dawu@pinduoduo.com>
2019-09-27 11:49:11 +0800

    fix: root delete sep

--- packages/ssh-pool/src/commands/cd.js
+++ packages/ssh-pool/src/commands/cd.js
@@ -7,8 +7,9 @@ export function formatCdCommand({ folder }) {
   requireArgs(['folder'], { folder }, 'cd')
   const args = ['cd', folder]
   const { root } = path.parse(folder)
+  const drive = root.replace(path.sep, '')
   if (isWin && root !== '/') {
-    args.push(`&& ${root}`)
+    args.push(`&& ${drive}`)
   }
   return joinCommandArgs(args)
 }
